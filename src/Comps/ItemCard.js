86a2d255b91dce43f8476e9b268454067330d64d
evboggs302 <evboggs94@gmail.com>
2021-01-31 08:58:56 -0500

    staging for editting an item

--- src/Comps/ItemCard.js
+++ src/Comps/ItemCard.js
@@ -3,10 +3,14 @@ import React, { useEffect, useState } from "react";
 export default function ItemCard(props) {
   const { name, index, deleteFunc } = props;
   let deleteMe = () => deleteFunc(index);
+  const [editBool, changeBool] = useState(false);
+  const [itemVal, updateItem] = useState(name)
+  const editVal = () => changeBool(true);
   return (
     <li>
-      <div>{name}</div>
+      {editBool ? <input type="text" /> : <div>{name}</div>}
       <button onClick={deleteMe}>Delete Item</button>
+      <button onClick={editVal}>Edit Item</button>
     </li>
   );
 }
